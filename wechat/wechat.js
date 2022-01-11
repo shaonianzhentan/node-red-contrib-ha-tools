a7a7e8ffcb6d9cdb3954b951f88dc90817e0f4dc
shaonianzhetnan <5294022+shaonianzhentan@users.noreply.github.com>
2022-01-11 15:46:13 +0800

    消息临时存储

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -5,12 +5,33 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         this.server = RED.nodes.getNode(config.server);
         if (this.server) {
+            // 消息临时存储
+            const list = []
             const node = this
             const { uid } = config
             node.on('input', async function (msg) {
                 try {
                     const message = CryptoUtil.decrypt(msg.payload, uid)
-                    const { message_id, content } = JSON.parse(message)
+                    const { message_id, time, content } = JSON.parse(message)
+                    // 判断是否过期
+                    const second = Math.round((Date.now() - time) / 1000)
+                    if (second > 5) {
+                        return node.status({ fill: "red", shape: "ring", text: "消息过期，拒绝执行" });
+                    }
+                    // 判断是否重复
+                    if (list.findIndex(ele => ele.message_id === message_id) >= 0) {
+                        return node.status({ fill: "red", shape: "ring", text: "消息重复，拒绝执行" });
+                    } else {
+                        // 10秒后删除临时标识
+                        list.filter(ele => Math.round((Date.now() - ele.time) / 1000) > 10).forEach(ele => {
+                            const index = list.findIndex(item => item.message_id === ele.message_id)
+                            list.splice(index, 1)
+                        })
+                    }
+                    console.log('消息队列', list.length)
+                    // 加入消息队列
+                    list.push({ message_id, time })
+
                     const { speech } = await this.server.conversation(content)
                     node.send({
                         topic: `shaonianzhentan/homeassistant/${message_id}`,
